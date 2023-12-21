7dcc9bcfc3d8c8d2582a60bc43e50bea833323f2
Scrice <davidemassimo94@gmail.com>
2023-12-21 17:11:35 +0100

    chore(tests): create a new folder utils for testAPI

--- tests/api/userAPI.test.ts
+++ tests/api/userAPI.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import * as testUtils from "./mongoTestUtils";
+import * as testUtils from "./utils/mongoTestUtils";
 
 describe("unit", () => {
     describe("userAPI", () => {
