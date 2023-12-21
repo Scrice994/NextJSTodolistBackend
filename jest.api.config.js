2ed72e512042b3f0224f2b109ca804158c3f2ddb
Scrice <davidemassimo94@gmail.com>
2023-12-21 17:12:46 +0100

    fix(tests): updated setup path for jest.api.config

--- jest.api.config.js
+++ jest.api.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/api/*.test.ts'],
-  setupFilesAfterEnv: ["<rootDir>/tests/api/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/tests/api/utils/setup.ts"],
   maxWorkers: 1
 };
